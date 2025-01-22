6bf2097db2b6c79e1f11f8122187523c7d100f88
Ron Deppe <97666744+RonDeppe@users.noreply.github.com>
2025-01-22 23:30:21 +0100

    0.0.1 - Added the sleeping functionality.

--- main.go
+++ main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"syscall"
+	"time"
 )
 
 func main() {
-	
+	beforeSleep := time.Now()
+
+	if !sleep() {
+		return
+	}
+
+	afterSleep := time.Now()
+
+	duration := afterSleep.Sub(beforeSleep)
+
+	fmt.Printf("System went to sleep at: %v\n", beforeSleep)
+	fmt.Printf("System woke up at: %v\n", afterSleep)
+	fmt.Printf("Elapsed time: %v\n", duration)
 }
+
+func sleep() bool {
+	powrProf := syscall.NewLazyDLL("powrprof.dll")
+	setSuspendState := powrProf.NewProc("SetSuspendState")
+
+	// 0 = Sleep, 0 = No Hibernate, 0 = Allow Wake Events
+	ret, _, err := setSuspendState.Call(0, 0, 0)
+	if ret == 0 {
+		fmt.Printf("Failed to put the system to sleep: %v\n", err)
+		return false;
+	}
+
+	return true
+}
+
